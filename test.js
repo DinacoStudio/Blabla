a04e018456be118a722701756f33fcb8ea47b99d
Dinaco Studio <67733346+DinacoStudio@users.noreply.github.com>
2023-01-18 20:09:26 +0500

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const Balaboba = require('./balaboba');
-const balaboba = new Balaboba();
-
-balaboba.generate('Данил Прытков пришёл',0).then(response => {
-    console.log(response);
-    console.log(1)
-});
